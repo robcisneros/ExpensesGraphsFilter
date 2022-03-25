541b95e2b5aa2bb6838d227b0f50065cbd36269f
robcisneros <robertocisneroslp@gmail.com>
2022-03-25 12:13:47 -0600

    console log en expenseChart

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -18,8 +18,9 @@ const ExpensesChart = (props) => {
     { label: 'Dec', value: 0 },
   ];
 
+  
   for (const expense of props.expenses) {
-    const expenseMonth = expense.date.getMonth(); // starting at 0 => January => 0
+    const expenseMonth = expense.date.getMonth(); // obtiene el valor del mes de cada expense
     chartDataPoints[expenseMonth].value += expense.amount;
   }
 
